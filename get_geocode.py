402dbd091adcfcb353463295f3e4d4ee93d2d086
Ilshat Fattakhov <i.fattahov@gmail.com>
2016-01-11 17:46:58 +0500

    Modifies geocoding script

--- get_geocode.py
+++ get_geocode.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 import sys
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
@@ -13,13 +14,15 @@ def get_geodata(url, addr_str):
         request_result = opener.open(request)
         the_page = request_result.read()
         response = json.loads(the_page)
+        #fordebug
+        #print response
         if int(response['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'])>0:
             for k in response['response']['GeoObjectCollection']['featureMember']:
-                if k['GeoObject']['metaDataProperty']['GeocoderMetaData']['precision']=='exact':
+                if k['GeoObject']['metaDataProperty']['GeocoderMetaData']['precision'] in ['exact','street','near','number']:
                     po = k['GeoObject']['Point']['pos'].split(" ")
                     return '['+po[1]+','+po[0]+']'
-                #else:
-                #    print "no coordinates were found for address "+addr_str
+                else:
+                    print "no coordinates were found for address "+addr_str
             #print "Found"
         else:
             print addr_str
@@ -32,7 +35,7 @@ if __name__ == "__main__":
     #Reading arguments
     api_url = 'https://geocode-maps.yandex.ru/1.x/?format=json&geocode='
 
-    json_file_path = '/home/ilshat/work/LPC/ilshatfattakhov.github.io/data/deliveries_spb.json'
+    json_file_path = './data/deliveries_spb.json'
     fp = open(json_file_path, 'r')
     #print fp.read()
     json_value = fp.read()
@@ -43,7 +46,7 @@ if __name__ == "__main__":
         address_list.append('Saint Petersburg, '+a.get('street','')+", "+a.get('house','')+"/"+a.get('corps','')+", "+a.get('apartment',''))
         
 
-    #print get_geodata(url = api_url, addr_str = str("Magnitogorsk, Lenina, 58"))    
+    #print get_geodata(url = api_url, addr_str = str("Saint Petersburg, Альпийский пер, 15-1/, 173 "))    
     fdata = open('static/data_spb.js','w')
     fdata.write('var data = [')
     
@@ -54,7 +57,7 @@ if __name__ == "__main__":
 
     #print data 
     for k in range(0,len(data)):
-        print address_list[k], data[k] 
+        #print address_list[k], data[k] 
         if data[k]<>None:
             fdata.write(str(data[k]))
             fdata.write(',')
